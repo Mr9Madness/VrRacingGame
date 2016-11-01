1d6ade0ae24975452e596d1f54ef0543600d7936
Christiaan Sommer <chris241@hotmail.nl>
2016-11-01 15:52:08 +0100

    editor dingen, model dingen, tilemap inprovements

---
+++ VrRacingGame/Assets/Editor/LevelGen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor( typeof( TileMap ) )]
+public class LevelGen : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if ( GUILayout.Button( "ReLoad" ) )
+        {
+            TileMap map = ( TileMap )target;
+            map.InitLevel();
+        }
+        if( GUILayout.Button( "Empty" ) )
+        {
+            TileMap map = ( TileMap ) target;
+            map.EmptyLevel();
+        }
+    }
+}
--- VrRacingGame/Assets/Scripts/TileMap.cs
+++ VrRacingGame/Assets/Scripts/TileMap.cs
@@ -4,29 +4,44 @@ using UnityEngine;
 public class TileMap : MonoBehaviour
 {
     private LevelData _tileData;
+
     public GameObject[] Trackparts;
+    public Transform TrackContainer;
 
     public string MapName;
 
     void Start()
-    {
-        MapName = "level1";
-
-        _tileData = LevelData.CreateFromJson( MapName );
-        
+    {        
         InitLevel();
     }
 
-    void InitLevel()
+    public void InitLevel()
     {
-        for ( int x = 0; x <= _tileData.LevelWidth; x++ )
+        _tileData = LevelData.CreateFromJson( MapName );
+
+        if ( TrackContainer == null )
+            TrackContainer = new GameObject( "Track" ).transform;
+
+        EmptyLevel();
+
+        for( int x = 0; x < _tileData.LevelWidth; x++ )
         {
-            for ( int y = 0; y <= _tileData.LevelHeight; y++ )
+            for ( int y = 0; y < _tileData.LevelHeight; y++ )
             {
                 int tileValue = _tileData.GetTile( x, y );
-                Instantiate( Trackparts[ tileValue ], new Vector3( x * 31.5f, 0, y * 31.5f ), Quaternion.identity );
+                Instantiate( Trackparts[ tileValue ], new Vector3( x * 31.5f, 0, y * 31.5f ), Quaternion.Euler( 0, tileValue * 90, 0 ), TrackContainer );
             }
         }
     }
 
+    public void EmptyLevel()
+    {
+        if( TrackContainer == null ) return;
+        if( TrackContainer.childCount <= 0 ) return;
+
+        while( TrackContainer.childCount > 0 )
+            foreach( Transform child in TrackContainer )
+                DestroyImmediate( child.gameObject );
+        
+    }
 }
